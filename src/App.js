976643bb975ade8007902b1eb5cf6aa66014fc11
Erik Sandoval <erik.sandoval@icloud.com>
2019-11-13 15:53:04 -0800

    initial render of map, with 4 layers, and layer switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,80 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
+import { loadModules } from 'esri-loader';
+import './App.scss'
 
-import './App.scss';
+const App = () => {
+  const mapRef = useRef();
 
-function App() {
-  return (
-    <div className="App">
-    </div>
+  useEffect(
+    () => {
+      // lazy load the required ArcGIS API for JavaScript modules and CSS
+      loadModules(['esri/Map',
+        'esri/views/MapView',
+        "esri/layers/GeoJSONLayer",
+        "esri/PopupTemplate",
+        "esri/widgets/Popup",
+        "esri/widgets/LayerList"],
+        { css: true })
+        .then(([ArcGISMap, MapView, GeoJSONLayer, PopupTemplate, Popup,
+          LayerList]) => {
+
+
+          const statesTemplate = new PopupTemplate({
+            content: "hello"
+          })
+          const usOutline = new GeoJSONLayer({
+            title: "United States",
+            url: './us_outline.json'
+          })
+          const usStates = new GeoJSONLayer({
+            title: "States",
+            url: './us_states.json',
+            PopupTemplate: statesTemplate
+          })
+          const usCongressional = new GeoJSONLayer({
+            title: "United States Congressional",
+            url: './us_congressional.json'
+          })
+          const usCounties = new GeoJSONLayer({
+            title: "United States Counties",
+            url: './us_counties.json'
+          })
+
+          const map = new ArcGISMap({
+            basemap: 'gray',
+            layers: [usOutline, usStates]
+          });
+
+          // load the map view at the ref's DOM node
+          const view = new MapView({
+            container: mapRef.current,
+            map: map,
+            center: [-100.233554, 37.426343],
+            zoom: 5,
+          });
+
+          const layerList = new LayerList({
+            view: view
+          })
+
+          view.ui.add(layerList, {
+            position: "top-right"
+          })
+
+          view.popup.on('click', function (evt) {
+            console.log(evt)
+          })
+
+          return () => {
+            if (view) {
+              // destroy the map view
+              view.container = null;
+            }
+          };
+        });
+    }
   );
-}
 
-export default App;
+  return <div className="webmap" ref={mapRef} />;
+}
+export default App
